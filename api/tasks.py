ba485d45e3581512137e00ff422896c7c68716f5
Khvostenko-OV <xboctehko@yandex.ru>
2026-02-09 19:23:15 +0100

    list parsing

--- api/tasks.py
+++ api/tasks.py
@@ -3,7 +3,7 @@ import threading
 
 from celery import shared_task
 
-from TG_client.utils import send_results
+from TG_client.utils import send_results, send_chats, save_json
 from accounts.models import User
 from params.models import Log
 from tasks.models import TGgroup
@@ -48,6 +48,43 @@ def parsing_group(group_pk: int, admin_pk: int, url: str, start=0, end=0):
     if error:
         Log.set(f"API: [{admin}] parsing tg-chat '{group.title}'. Error: {error}")
         err = send_results(url, info=info, error=f"TG-parsing chat '{group}' Error: {error}")
+        if err:
+            Log.set(f"API: [{admin}] Error: Can't send error report -> {err}")
+
+    future = asyncio.run_coroutine_threadsafe(admin.disconnect(), loop)
+    result = future.result()
+    return
+
+
+@shared_task
+def parsing_list(link: str, admin_pk: int, url: str):
+    admin = User.get(admin_pk)
+    if not all([link, admin, url]): return
+    Log.set(f"API: [{admin}] start parsing invite-list '{link}'")
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    threading.Thread(target=loop.run_forever, daemon=True).start()
+    try:
+        future = asyncio.run_coroutine_threadsafe(admin.connect(loop), loop)
+        result = future.result()
+        if not result: raise Exception(f"TG auth error")
+
+        future = asyncio.run_coroutine_threadsafe(admin.parse_list(link), loop)
+        result = future.result()
+        if result["error"]: raise Exception(result["error"])
+        chats = result["chats"]
+#        save_json(chats, link)
+        Log.set(f"API: [{admin}] from invite-link '{link}' parsed chats - {len(chats)}")
+        err = send_chats(url, chats=chats)
+        if err:
+            Log.set(f"API: [{admin}] Error: Can't send results -> {err}")
+        else:
+            Log.set(f"API: [{admin}] {len(chats)} chats sent to {url}")
+
+    except Exception as err:
+        error = str(err)
+        Log.set(f"API: [{admin}] parsing invite-link '{link}'. Error: {error}")
+        err = send_chats(url, error=error)
         if err:
             Log.set(f"API: [{admin}] Error: Can't send error report -> {err}")
 
--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
-from api.views import group_add, group_delete, group_parse
+from api.views import group_add, group_delete, group_parse, list_parse
 
 urlpatterns = [
     path("group/add/", group_add, name="api_group_add"),
     path("group/delete/", group_delete, name="api_group_del"),
     path("group/parse/", group_parse, name="api_group_parse"),
-    path("group/get/", group_delete, name="api_group_parse"),
+    path("group/list/", list_parse, name="api_list_parse"),
     # path("task/add/", group_delete, name="api_task_add"),
     # path("task/delete/", group_delete, name="api_task_del"),
     # path("task/update/", group_delete, name="api_task_change"),
